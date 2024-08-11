1f61a022dcfe9636db055492485bb542d378af34
Matthew Lee <ml8423799@gmail.com>
2024-08-11 20:27:54 +0700

    neat code, install lite-server, update

--- test/ap.js
+++ test/ap.js
@@ -1,22 +1,33 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+alert("This is Only Demo ")
+
+var attempt = 3;
+
 function validate(){
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-if ( username == "Tiga Bintang Kreasi" && password == "Tigabintangkreasi123"){
-alert ("Login successfully");
-window.location = "../index.html"; // Redirecting to other page.
-return false;
-}
-else{
-attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
-document.getElementById("username").disabled = true;
-document.getElementById("password").disabled = true;
-document.getElementById("submit").disabled = true;
-return false;
-}
-}
+   var username = document.getElementById("username").value;
+   var password = document.getElementById("password").value;
+
+   if(username == "" || password == ""){
+        alert("please input username and password")
+        return false;
+   }
+
+   if ( username == "Tiga Bintang Kreasi" && password == "Tigabintangkreasi123"){
+       alert ("Login successfully");
+       window.location = "../HTML/home.html"; // Redirecting to other page.
+       return false;
+   }
+
+   else{
+     attempt --;
+     alert("You have left " + attempt + " attempt;");
+     document.getElementById('username').value = ""
+     document.getElementById("password").value = "";
+     
+     if(attempt == 0){
+       document.getElementById("username").disabled = true;
+       document.getElementById("password").disabled = true;
+       document.getElementById("submit").disabled = true;
+       return false;
+     }
+  }
 }
